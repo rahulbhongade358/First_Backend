8823a168c569862ce4180fe3d768f0c115593fe7
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-08-27 20:08:53 +0530

    added .env file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
  const app = express()
  app.use(express.json())
  const REGISTERED_USERS =[
@@ -188,6 +190,7 @@ res.json({
         message:"User Edit Successfully"
     })
  })
- app.listen(8080,()=>{
-    console.log("server is running on port 8080")
+ const PORT = process.env.PORT||5003
+ app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
  })
