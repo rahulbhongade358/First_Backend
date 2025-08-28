6739accd463d7f965a88f88283598967ef525e38
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-08-28 20:35:20 +0530

    cors setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv"
+import cors from "cors";
 dotenv.config()
  const app = express()
  app.use(express.json())
+    app.use(cors())
  const REGISTERED_USERS =[
      {ID:1,
         firstName:"Rahul",
